4ba66d2a0c5d104ad8cafacb47027eb5f0ec46f6
Langston Barrett <langston.barrett@gmail.com>
2023-03-12 10:18:54 -0400

    test: Add match-c script

--- test/const-hex.c
+++ test/const-hex.c
@@ -1,4 +1,4 @@
-// RUN: treereduce-c -q -j 1 -o - -s %s -- grep -E 'long NUM =' 2>&1 | FileCheck %s
+// RUN: treereduce-c -q -j 1 -o - -s %s -- match-c NUM 2>&1 | FileCheck %s
 
 // CHECK: NUM = 0;
 const long NUM = 0xbad1dea;
