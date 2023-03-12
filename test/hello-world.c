f7698a51bf52a60580d6310e2f0aba94bdf7fcb4
Langston Barrett <langston.barrett@gmail.com>
2023-03-12 09:41:48 -0400

    test: Make hello-world more stringent

--- test/hello-world.c
+++ test/hello-world.c
@@ -1,7 +1,7 @@
 // RUN: treereduce-c -j 1 -o - -s %s -- clang -o /dev/null @@.c 2>&1 | FileCheck %s
 
 #include <stdio.h>
-// CHECK: main
+// CHECK: main()
 int main(int argc, char *argv[]) {
 // CHECK-NOT: puts
   puts("Hello, world!\n");
