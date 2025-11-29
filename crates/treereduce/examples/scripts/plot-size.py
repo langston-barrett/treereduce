ab77d4de46b28fa27118fd09bead90fd36fe7cb5
Langston Barrett <langston.barrett@gmail.com>
2025-11-29 17:49:30 +0000

    Lint and format

--- crates/treereduce/examples/scripts/plot-size.py
+++ crates/treereduce/examples/scripts/plot-size.py
@@ -1,8 +1,18 @@
+"""Create plots for benchmarks"""
+
+from argparse import ArgumentParser
 import seaborn as sns
 import pandas as pd
 
-cols = ["tool", "vers", "test", "jobs", "conf", "file", "start", "end size", "time"]
-data = pd.read_csv("data.csv", names=cols)
-sns.set_theme()
-plot = sns.catplot(data=data, kind="bar", x="tool", y="end size")
-plot.fig.savefig("out.svg") 
+
+def go() -> None:
+    cols = ["tool", "vers", "test", "jobs", "conf", "file", "start", "end size", "time"]
+    data = pd.read_csv("data.csv", names=cols)
+    sns.set_theme()
+    plot = sns.catplot(data=data, kind="bar", x="tool", y="end size")
+    plot.fig.savefig("out.svg")
+
+
+parser = ArgumentParser(description=__doc__)
+parser.parse()
+go()
